940b47e5b12d4ce2bdd2b99e771321aeb4b7b174
Noah2211 <noah.decroix3@gmail.com>
2022-01-21 10:59:35 +0100

    done

--- index.php
+++ index.php
@@ -9,9 +9,19 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const nbr = 12;
+const nbr2 = 2;
 
+echo nbr * nbr2 ."<br><br>";
+
+const str = "C'est cool php";
+
+echo (nbr + nbr2) * strlen(str)."<br><br>";
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+echo basename(__DIR__)."<br>";
+echo basename(__FILE__)."<br>";
+echo basename(__LINE__)."<br>";
